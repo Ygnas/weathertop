20f839729198463ec1ab1a80e485aa162b8e3bbe
Ygnas <ygnas@outlook.com>
2022-05-18 14:08:18 +0100

    Now loads data by logged in member

--- app/controllers/Dashboard.java
+++ app/controllers/Dashboard.java
@@ -1,5 +1,6 @@
 package controllers;
 
+import models.Member;
 import models.Station;
 import play.Logger;
 import play.mvc.Controller;
@@ -9,19 +10,21 @@ import java.util.List;
 public class Dashboard extends Controller {
   public static void index() {
     Logger.info("Rendering Dashboard");
-
-    List<Station> stations = Station.findAll();
+    Member member = Accounts.getLoggedInMember();
+    List<Station> stations = member.stations;
     for (Station station : stations) {
       station.updateLatestData();
     }
     render("dashboard.html", stations);
   }
 
-  public static void addStation(String name)
+  public static void addStation(String name,double latitude,double longitude)
   {
-    Station station = new Station(name);
+    Station station = new Station(name,latitude,longitude);
     Logger.info ("Adding a new station called " + name);
-    station.save();
+    Member member = Accounts.getLoggedInMember();
+    member.stations.add(station);
+    member.save();
     redirect ("/dashboard");
   }
 }
