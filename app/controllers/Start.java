0a0ebb558decad6970d1e0782c55a6b376478bbe
Ygnas <ygnas@outlook.com>
2022-05-18 14:07:33 +0100

    Index to check if logged in

--- app/controllers/Start.java
+++ app/controllers/Start.java
@@ -1,11 +1,13 @@
 package controllers;
 
+import models.Member;
 import play.Logger;
 import play.mvc.Controller;
 
 public class Start extends Controller {
   public static void index() {
     Logger.info("Rendering Start");
+    Member member = Accounts.getLoggedInMember();
     render("start.html");
   }
 }
